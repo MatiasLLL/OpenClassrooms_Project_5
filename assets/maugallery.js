de102b5e37152c6d89ec2f2831a381769ea7109a
MatiasLLL <matias.larralde97@gmail.com>
2023-05-21 15:23:50 +0200

    Ajustement style bordure modale

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -204,12 +204,12 @@
                     <div class="modal-content">
                         <div class="modal-body">
                             ${navigation
-          ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
+          ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-4px;background:white;"><</div>'
           : '<span style="display:none;" />'
         }
                             <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
                             ${navigation
-          ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
+          ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-4px;background:white;}">></div>'
           : '<span style="display:none;" />'
         }
                         </div>
